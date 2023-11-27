002cb10e38f123db7ca453fabab0db2f37940a65
joy111joy <150044778+joy111joy@users.noreply.github.com>
2023-11-27 01:29:36 -0330

    Add files via upload

---
+++ Web_design.js
@@ -0,0 +1,61 @@
+const motelCustomer = {
+    firstName: "John",
+    lastName: "Doe",
+    title: "Mr",
+    gender: "Male",
+    birthday: "2000-10-06",
+    roomPref: ["111", "103", "119"],
+    payMethod: "Credit",
+    Address: {
+        streetAddress: "123 Easy Street",
+        City: "Townville",
+        Province: "NL",
+        PostalCode: "A1A 1A1"
+    },
+    phoneNum: "(123)654-7890",
+    checkInOutDates: 
+    {   checkIn: new Date(2023, 10, 17),
+        checkOut: new Date(2023, 10, 21),
+    },
+    checkInFormat: function(){
+        const checkInFormatted = this.checkInOutDates.checkIn.toISOString().slice(0,10)
+        return checkInFormatted
+    },
+    checkOutFormat: function(){
+        const checkOutFormatted = this.checkInOutDates.checkOut.toISOString().slice(0,10)
+        return checkOutFormatted
+    },
+    stayDuration: function(){
+
+        const Duration = this.checkInOutDates.checkOut - this.checkInOutDates.checkIn
+
+        durationInDays = Math.ceil(Duration/ (1000 * 60 * 60 * 24)) // converts milliseconds (/1000)to seconds (/60) to minutes (/60) to hours(/60) to days (/24)
+
+        return durationInDays
+    },
+    // get age using a func
+    custAge: function (){
+        const birthYear = parseInt(this.birthday.slice(0,4))
+        const today = new Date();
+        const todayYear = today.getFullYear()
+        const age = todayYear - birthYear
+        return age
+    }
+};
+
+
+let roomPrefDisp = motelCustomer.roomPref.join(", ");
+
+let CustInfo = `${motelCustomer.firstName} ${motelCustomer.lastName} will be staying with us on ${motelCustomer.checkInFormat()}. \
+${motelCustomer.title}. ${motelCustomer.lastName}'s stay will last ${motelCustomer.stayDuration()} days and will be checking out on \
+${motelCustomer.checkOutFormat()}. The customer has expressed their preference over the rooms ${roomPrefDisp} and will \
+be paying for their stay using ${motelCustomer.payMethod}. ${motelCustomer.title}. ${motelCustomer.lastName} is ${motelCustomer.custAge()} \
+years old. They live in ${motelCustomer.Address.Province} their address is ${motelCustomer.Address.streetAddress}, \
+${motelCustomer.Address.City}. Their phone number is ${motelCustomer.phoneNum} and their postal code is ${motelCustomer.Address.PostalCode}. \
+Please give ${motelCustomer.title}. ${motelCustomer.lastName} the best possible stay by catering the their every possible request.
+`;
+
+console.log(CustInfo)
+
+
+document. body. innerHTML = CustInfo;
