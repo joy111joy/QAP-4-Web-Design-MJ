42c75fc6d4b952989da9c6f225b0703c980dcdd8
Mitchel Joy <mitchel_joy@hotmail.com>
2023-11-27 02:03:32 -0330

    Added comments to the JS file to explain the code and what each line of code does.

--- Web_design.js
+++ Web_design.js
@@ -1,3 +1,8 @@
+// This file contains the code for the motel customer object and the code to display the customer's information on the webpage
+// Date: 11/23/2023 - 11/27/2023
+// Written by: Mitchel Joy
+
+// Set up the motel customer object
 const motelCustomer = {
     firstName: "John",
     lastName: "Doe",
@@ -6,7 +11,7 @@ const motelCustomer = {
     birthday: "2000-10-06",
     roomPref: ["111", "103", "119"],
     payMethod: "Credit",
-    address: {
+    address: { // sub object for specific address access
         streetAddress: "123 Easy Street",
         city: "Townville",
         province: "NL",
@@ -17,15 +22,15 @@ const motelCustomer = {
     {   checkIn: new Date(2023, 10, 17),
         checkOut: new Date(2023, 10, 21),
     },
-    checkInFormat: function(){
+    checkInFormat: function(){ // function to format the check in date by slicing the date to only include the year, month, and day
         const checkInFormatted = this.checkInOutDates.checkIn.toISOString().slice(0,10)
         return checkInFormatted
     },
-    checkOutFormat: function(){
+    checkOutFormat: function(){ // function to format the check out date by slicing the date to only include the year, month, and day
         const checkOutFormatted = this.checkInOutDates.checkOut.toISOString().slice(0,10)
         return checkOutFormatted
     },
-    stayDuration: function(){
+    stayDuration: function(){ // function to calculate the duration of the stay using the check in and check out dates
 
         const Duration = this.checkInOutDates.checkOut - this.checkInOutDates.checkIn
 
@@ -34,7 +39,7 @@ const motelCustomer = {
         return durationInDays
     },
     // get age using a func
-    custAge: function (){
+    custAge: function (){ // function to calculate the age of the customer using current year and birth year
         const birthYear = parseInt(this.birthday.slice(0,4))
         const today = new Date();
         const todayYear = today.getFullYear()
@@ -43,8 +48,10 @@ const motelCustomer = {
     }
 };
 
-
+// variable to format the room preference array into a string
 let roomPrefDisp = motelCustomer.roomPref.join(", ");
+
+// Display of the motel customer's information
 
 let CustInfo = `<p>${motelCustomer.firstName} ${motelCustomer.lastName} will be staying with us on ${motelCustomer.checkInFormat()}. \
 ${motelCustomer.title}. ${motelCustomer.lastName}'s stay will last ${motelCustomer.stayDuration()} days and will be checking out on \
@@ -57,5 +64,5 @@ Please give ${motelCustomer.title}. ${motelCustomer.lastName} the best possible 
 
 console.log(CustInfo)
 
-
+// Display the motel customer's information on the webpage
 document. body. innerHTML = CustInfo;
